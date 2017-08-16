5c62828771a0a28aa38c69548c364830560947cb
Sebastian Graz <sebastian.graz@spindriftgroup.com>
2017-08-16 11:04:35 +0100

    v1

--- app.vue
+++ app.vue
@@ -4,19 +4,32 @@
         <h1 class="fw8 mt5">{{ msg }}</h1>
         <div class="flex justify-between mt5">
           <label for="">{{ label }}</label>
-          <a class="link" href="#" v-on:click="runRandomHex">🔮 Random</a>
+          <a class="link" href="#" @click="runRandomHex">🔮 Random</a>
         </div>
 
         <div class="relative mt3">
           <span class="absolute top-1 left-1 black-50">#</span>
-          <input class="w-100 pv3 pl4 input-reset ba b--black-20" v-on:keyup="getColor(colorValue)" v-model="colorValue" placeholder="0AD674" >
+          <input class="w-100 pv3 pl4 input-reset ba b--black-20" @keyup="getColor(colorValue)" v-model="colorValue" placeholder="0AD674" >
         </div>
 
-        <div id="colorbox_darker" class="pa3 white w-100"></div>
+        <!-- <div id="colorbox_darker" class="pa3 white w-100"></div>
         <div id="colorbox_dark" class="pa3 white w-100"></div>
         <div id="colorbox_default" class="pa3 white w-100"></div>
         <div id="colorbox_light" class="pa3 white w-100"></div>
-        <div id="colorbox_lighter" class="pa3 white w-100"></div>
+        <div id="colorbox_lighter" class="pa3 white w-100"></div> -->
+
+        <ul class="list">
+          <li :id="item.id" v-for="item in items" :style="" class="pa3 w-100">
+
+          </li>
+        </ul>
+
+        <code class="dib w-100 bg-black-10 ma0 pa3">
+.color {
+    color: #{{ colorValue }};
+}
+        </code>
+
     </div>
   </div>
 </template>
@@ -42,40 +55,61 @@ export default {
     return {
       msg: 'Color generator',
       label: 'Enter your color',
-      colorValue: this.generateRandomHex()
+      colorValue: '4e35e1',
+      items: [
+            {
+              intensity: 3,
+              colorProperty: 'darken',
+              id: 'colorbox_darker'
+            },
+            {
+              intensity: 1,
+              colorProperty: 'darken',
+              id: 'colorbox_dark'
+            },
+            {
+              intensity: 0,
+              colorProperty: '',
+              id: 'colorbox_default'
+            },
+            {
+              intensity: 1,
+              colorProperty: 'brighten',
+              id: 'colorbox_light'
+            },
+            {
+              intensity: 3,
+              colorProperty: 'brighten',
+              id: 'colorbox_lighter'
+            }
+          ],
     }
   },
   mounted:function(){
       this.getColor(this.colorValue)
   },
   methods: {
-    runRandomHex: function (event) {
-      this.getColor(this.generateRandomHex());
+    runRandomHex: function () {
+      this.colorValue = this.generateRandomHex();
+      this.getColor(this.colorValue)
     },
-    generateRandomHex: function (event) {
-      return Math.random().toString(16).slice(2, 8).toUpperCase();
+    generateRandomHex: function () {
+      var length = 6;
+      var chars = '0123456789ABCDEF';
+      var hex = '';
+      while(length--) hex += chars[(Math.random() * 16) | 0];
+      return hex;
     },
-    getColor: function (color, event) {
-
+    getColor: function (color) {
       var color_darker = chroma(color).darken(3);
       var color_dark = chroma(color).darken(1);
       var color_default = chroma(color);
       var color_light = chroma(color).brighten(1);
       var color_lighter = chroma(color).brighten(3);
 
-      var colorBoxes = {
-        colorbox_darker : document.querySelector("#colorbox_darker"),
-        colorbox_dark : document.querySelector("#colorbox_dark"),
-        colorbox_default : document.querySelector("#colorbox_default"),
-        colorbox_light : document.querySelector("#colorbox_default"),
-        colorbox_lighter : document.querySelector("#colorbox_lighter")
-      }
-
       // for (let [key, value] of Object.entries(colorBoxes)) {
       //   Object.assign(colorbox_darker.style,{backgroundColor: value});
       // }
-
-
 
       colorbox_darker.style.backgroundColor = color_darker
       colorbox_dark.style.backgroundColor = color_dark
@@ -83,6 +117,7 @@ export default {
       colorbox_light.style.backgroundColor = color_light
       colorbox_lighter.style.backgroundColor = color_lighter
 
+      colorbox_darker.style.color = chroma(color_lighter).brighten(1)
       colorbox_light.style.color = chroma(color_lighter).darken(3)
       colorbox_lighter.style.color = chroma(color_lighter).darken(3)
 
